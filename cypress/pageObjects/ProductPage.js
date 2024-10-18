20ced58405601aad212a40ccddcc6eb44db25560
Agatha Pereira <95726716+htttaa@users.noreply.github.com>
2024-10-18 14:34:35 -0300

    Update ProductPage.js

--- cypress/pageObjects/ProductPage.js
+++ cypress/pageObjects/ProductPage.js
@@ -1,6 +1,6 @@
 class ProductPage {
   addItemToCart() {
-    cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(3)').click(); // Ajuste o seletor conforme o produto específico
+    cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(3)').click();
     cy.get('.fixedBtn > .roboto-medium').click();
   }
 }
